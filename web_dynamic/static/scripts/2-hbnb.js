afba2f8330748a952bbf94bbfafa43e64c727fb2
yusufolola <yusufoyesanmi@yahoo.com>
2024-08-12 05:35:21 +0100

    all in place

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,18 @@
+$(document).ready(init);
+
+const HOST = '0.0.0.0';
+
+function init () {
+  const amenityObj = {};
+  $('.amenities .popover input').change(function () {
+    if ($(this).is(':checked')) {
+      amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      delete amenityObj[$(this).attr('data-name')];
+    }
+    const names = Object.keys(amenityObj);
+    $('.amenities h4').text(names.sort().join(', '));
+  });
+
+  apiStatus();
+}
